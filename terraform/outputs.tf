f91302af925f47e0808cf243e84c778a531c041b
Konstantinos Konstantinidis <konstantinos@cloudvisor.eu>
2026-03-14 12:21:17 +0200

    feat: add terraform outputs

---
+++ terraform/outputs.tf
@@ -0,0 +1,65 @@
+output "cluster_name" {
+  description = "Name of the EKS cluster"
+  value       = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  description = "Endpoint for EKS control plane"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_security_group_id" {
+  description = "Security group ID attached to the EKS cluster"
+  value       = module.eks.cluster_security_group_id
+}
+
+output "cluster_certificate_authority_data" {
+  description = "Base64 encoded certificate data required to communicate with the cluster"
+  value       = module.eks.cluster_certificate_authority_data
+  sensitive   = true
+}
+
+output "cluster_oidc_issuer_url" {
+  description = "The URL on the EKS cluster OIDC Issuer"
+  value       = module.eks.cluster_oidc_issuer_url
+}
+
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "private_subnets" {
+  description = "List of IDs of private subnets"
+  value       = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  description = "List of IDs of public subnets"
+  value       = module.vpc.public_subnets
+}
+
+output "karpenter_queue_name" {
+  description = "Name of the SQS queue for Karpenter spot interruption notifications"
+  value       = module.karpenter.queue_name
+}
+
+output "karpenter_node_instance_profile_name" {
+  description = "Name of the IAM instance profile for Karpenter nodes"
+  value       = module.karpenter.node_instance_profile_name
+}
+
+output "karpenter_node_iam_role_arn" {
+  description = "ARN of the IAM role for Karpenter nodes"
+  value       = module.karpenter.node_iam_role_arn
+}
+
+output "configure_kubectl" {
+  description = "Command to configure kubectl"
+  value       = "aws eks update-kubeconfig --region ${var.region} --name ${module.eks.cluster_name}"
+}
+
+output "karpenter_controller_role_arn" {
+  description = "ARN of the IAM role for Karpenter controller"
+  value       = module.karpenter.iam_role_arn
+}
